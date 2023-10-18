b41807be1c0daadd625c1efcb9fe2ca5b899bbcf
ArianMathai <123459777+ArianMathai@users.noreply.github.com>
2023-10-18 13:14:43 +0200

    Added cookie test

--- server/__tests__/snapBoard.test.js
+++ server/__tests__/snapBoard.test.js
@@ -3,32 +3,44 @@ import express, {response} from "express";
 import bodyParser from "body-parser";
 import * as dotenv from "dotenv";
 import {snapBoardRoutes} from "../controller/snapBoardRoutes.js";
-import { getUserByUsername} from "../repository/userDataAccess.js";
-import jest from "jest";
+import {getUserById, getUserByUsername} from "../repository/userDataAccess.js"
+
+import jwt from "jsonwebtoken";
+
+
+import cookieParser from "cookie-parser";
+import cookieJwtAuth from "../middleware/cookieJwtAuth.js";
 
 dotenv.config();
 
 const app = express();
+app.use(cookieParser())
 app.use(snapBoardRoutes);
 app.use(bodyParser.json())
-app.use("/api/snapboard", snapBoardRoutes);
+app.use("/api/snapboard", cookieJwtAuth, snapBoardRoutes);
+
 
 
 describe("Testing SnapBoard", () =>{
-
     it("Should post a message to the database.", async () =>{
 
+
         const user = await getUserByUsername(process.env.VALID_USERNAME);
+        console.log(user)
+
+        const token = jwt.sign(user,process.env.MY_SECRET, {expiresIn: "1h"});
+
+        console.log(token)
 
         const response = await request(app)
-            .post("/api/snapboard/")
-            .send({
-                message:"test",
-                time:"1",
-                user,
-            })
+            .post("/api/snapboard/postmessage")
+            .set('Cookie', `token=${token}`)
+            .set("Content-Type", "application/json")
+            .send({message:"Testish", time: "5"})
 
-        expect(response.status).toBe(200);
+
+        expect(response.status).toBe(200)
+        expect(response.body.message).toBe("Posted message to SnapBoard.")
 
     })
 })
@@ -38,18 +50,23 @@ describe("Testing SnapBoard", () =>{
 describe("Testing SnapBoard", () =>{
     it("Should return an array of messages.", async () =>{
 
+        const user = await getUserByUsername(process.env.VALID_USERNAME);
+
+        const token = jwt.sign(user,process.env.MY_SECRET, {expiresIn: "1h"});
+
         const response = await request(app)
             .get("/api/snapboard/getAllMessages")
+            .set('Cookie', `token=${token}`)
             .set("Content-Type", "application/json")
             .send();
 
         expect(response.status).toBe(200);
         expect(response.body.messages[0]).toEqual(
             expect.objectContaining({
-                _id:expect.stringMatching(/.*?/),
-                message:expect.stringMatching(/.*?/),
-                time:expect.stringMatching(/.*?/),
-                user:expect.stringMatching(/.*?/)
+                    _id:expect.stringMatching(/.*?/),
+                    message:expect.stringMatching(/.*?/),
+                    time:expect.stringMatching(/.*?/),
+                    user:expect.stringMatching(/.*?/)
                 }
             )
         )
