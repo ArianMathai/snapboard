4b3ec7f2d29bca9a0160c4224093ca8e1b8896a0
Simen Didrichsen <108331982+didrichsen@users.noreply.github.com>
2023-10-21 10:04:52 +0200

    updated welcome text.

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -4,12 +4,9 @@ function Home(){
     return(
         <div className={"home-container"}>
             <p className={"homeText"}>
-                Velkommen til det digitale anarkiet, et fristed for dine tanker og meninger hvor ingen ansiktstrekk avsløres.
-                Dette er din virtuelle maskerade, en helligdom for fri tanke. Her kan du kaste din stemme til vinden og la dine ord danse
-                anonymt blant de digitale skyggene. Respekt er nøkkelen, selv i anonymitetens skjul,
-                for vi tror på den usynlige sosiale kontrakten som binder oss alle sammen.
-                Bli en del av dette sanatoriumet for det usagte, men husk, selv i anonymitetens frihet, har ordene dine makt.
-                Trå varsomt i denne digitale villmarken.Velkommen til ville vesten.
+                Welcome to the digital realm, a sanctuary for your thoughts and opinions, where no facial expressions are revealed. This is your virtual masquerade, a haven for free thinking.
+                Here, you can cast your voice to the wind and let your words dance anonymously among the digital shadows.
+                Respect is key, even in the cloak of anonymity, so weigh the power of words.
             </p>
         </div>
     )
